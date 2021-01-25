0c2f97037e833e52dfa5987f3d3d9273d940a9cd
mxy3538 <mxy3538@qq.com>
2021-01-25 20:27:29 +0800

    初识C语言（1）

---
+++ 课上跟进/Project_01_25/Project_01_25/test.c
@@ -0,0 +1,79 @@
+
+
+#include <stdio.h>
+
+
+
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf_s("%d %d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+
+//int a = 100;
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	short age = 20;
+//	float weight = 95.6f;
+//	printf("%d,%f\n", age,weight);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	short age = 20;
+//	printf("%d\n", age);
+//	float weight = 95.6f;
+//	printf("%f\n", weight);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	//double d = 3.14;
+//	//printf("%lf\n", d);
+//
+//	//float c = 5.2;
+//	//printf("%f\n", c);
+//
+//	//int b = 20;
+//	//printf("%d\n", b);
+//
+//	//char a = 'A';
+//	//printf("%c\n",a);
+//
+//	//printf("hello world\n");
+//	
+//	return 0;
+//}
+
