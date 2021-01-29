44b8b91a2c7a6ce4308fcc8810f9da55a173de6e
mxy3538 <mxy3538@qq.com>
2021-01-30 00:01:30 +0800

    上传1月29号晚上12点的完结代码

---
+++ 课上跟进/Project_01_29/Project_01_29/test.c
@@ -0,0 +1,76 @@
+
+#include<stdio.h>
+int main()
+{
+	printf("%c\n", 26);
+	return 0;
+}
+
+
+//#include<string.h>
+//int main()
+//{
+//	printf("%d\n", strlen("E:\tode\32\test.c"));
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("E:\test\\test.c\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%c\n",'\'');
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("E:\code\test.c");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("E:\test\32\test.c");
+//	return 0;
+//}
+
+//#include<string.h>
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c' };
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c','\0'};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
