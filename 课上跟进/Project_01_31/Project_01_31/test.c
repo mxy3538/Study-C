f3daac90f26835706daef94a81d3cd0748a60c7e
mxy3538 <mxy3538@qq.com>
2021-01-31 22:14:12 +0800

    上传2021.1.31的课堂代码

---
+++ 课上跟进/Project_01_31/Project_01_31/test.c
@@ -0,0 +1,151 @@
+
+
+#include<stdio.h>
+
+
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a & b; int d = a ^ b; int e = a | b;
+//	printf("%d\n%d\n%d\n", c, d, e);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 1;
+//	int b = a << 2;
+//	int c = b >> 1;
+//	printf("%d\n", b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 5 / 2;//5除2--取商
+//	int b = 5 % 2;//5除2--取模(余数)
+//	printf("%d\n%d\n", a,b);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d ", arr[i]);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", arr[4]);
+//	return 0;
+//}
+
+
+//int Add(int x, int y)
+//{
+//    int z = x + y;
+//    return z;
+//}
+//int main()
+//{
+//    int num1 = 0;
+//    int num2 = 0;
+//    int sum = 0;
+//    printf("输入两个操作数:>");
+//    scanf("%d %d", &num1, &num2);
+//    sum = Add(num1, num2);
+//    printf("sum = %d\n", sum);
+//    return 0;
+//}
+
+
+
+
+//int main()
+//{
+//    int num1 = 0;
+//    int num2 = 0;
+//    int sum = 0;
+//    printf("输入两个操作数:>\n");
+//    scanf("%d %d", &num1, &num2);
+//    sum = num1 + num2;
+//    printf("sum = %d\n", sum);
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//	int line = 0;
+//	printf("加入比特\n");
+//	while (line <= 20000)
+//	{
+//		line++;
+//		printf("继续敲代码：%d\n",line);
+//	}
+//	if ("line>20000")
+//		printf("好offer\n");
+//	return 0;
+//}
+
+
+//#include<string.h>
+//int main()
+//{
+//	int input = 0;
+//	printf("你会坚持每天敲代码吗？（选择1 or 0）>：");
+//	scanf("%d", &input);
+//	if (input == 1)
+//	{
+//		printf("坚持，你会有好offer\n");
+//	}
+//	else
+//	{
+//		printf("放弃，回家卖红薯\n");
+//	}
+//	return 0;
+//}
+
+
+///*
+//int main()
+//{
+//	int a = 10;
+//	/*
+//	int b = 20;
+//	*/
+//	int c = 30;
+//	return 0;
+//}
+//*/
+
+
+//int main()
+//{
+//	printf("%c\n", '\x61');
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("E:\\test\\32\\test.c\n");
+//	return 0;
+//}
