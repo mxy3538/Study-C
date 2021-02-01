0f81796d06ef610fa0270b17e9c23a27eeb9822b
mxy3538 <mxy3538@qq.com>
2021-02-01 22:54:18 +0800

    上传初识C语言（2）最后的课堂代码

---
+++ 课上跟进/Project_02_01/Project_02_01/test.c
@@ -0,0 +1,21 @@
+
+
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	printf("%d\n", sizeof(a));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof a);
+	return 0;
+}
+
+
+
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", a);
+//	printf("%d\n", !a);
+//	return 0;
+//}
