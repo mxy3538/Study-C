16512332c28be3d4445e4e7379b9e6bb04cb22a8
mxy3538 <mxy3538@qq.com>
2021-02-02 21:54:05 +0800

    上传2021.2.2的代码

---
+++ 课上跟进/Project_02_02/Project_02_02/test.c
@@ -0,0 +1,44 @@
+
+
+
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
+
+
+////写一个函数求两个数的较大值
+//#include<stdio.h>
+//int Max(int x, int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int max = 0;
+//	max = Max(num1, num2);
+//	printf("较大值为：%d\n", max);
+//	return 0;
+//}
+
+
+
+////求两个数的较大值
+//#include<stdio.h>
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	if (num1 > num2)
+//		printf("较大值为：%d\n", num1);
+//	else
+//		printf("较大值为：%d\n", num2);
+//	return 0;
+//}
