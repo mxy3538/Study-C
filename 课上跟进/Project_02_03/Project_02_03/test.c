02cb5d983f41973db33c8e5cb45f68d538b86c00
mxy3538 <mxy3538@qq.com>
2021-02-03 21:12:42 +0800

    上传20210203代码

---
+++ 课上跟进/Project_02_03/Project_02_03/test.c
@@ -0,0 +1,31 @@
+
+
+#include<stdio.h>
+//int main()
+//{
+//	int a = 100;
+//	int b = 20;
+//	int max = 0;
+//	max = (a > b ? a:b);
+//	printf("max = %d\n", max);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int a = (int)3.14;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	//int b = ++a;
+//	int b = a++;
+//	printf("a = %d\nb = %d\n", a,b);
+//	return 0;
+//}
