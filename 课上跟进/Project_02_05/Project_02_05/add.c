dd636e25114cc4b07915ede846b79de5a3edeb88
mxy3538 <mxy3538@qq.com>
2021-02-05 19:26:50 +0800

    上传2021_02_05的课堂代码

---
+++ 课上跟进/Project_02_05/Project_02_05/add.c
@@ -0,0 +1,2 @@
+
+static int add1 = 2021;//add1在这里作全局变量
---
+++ 课上跟进/Project_02_05/Project_02_05/test.c
@@ -0,0 +1,55 @@
+
+
+
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	extern int add1;
+//	printf("add1 = %d\n", add1);
+//	return 0;
+//}
+
+
+
+
+//#include <stdio.h>
+//void test()
+//{
+//	static int a = 1;
+//	//static修饰局部变量，局部变量的生命周期变长
+//	a++;
+//	printf("a = %d\n", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 5)
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+
+//#include<stdio.h>
+//void test()
+//{
+//	int a = 1;
+//	a++;
+//	printf("a = %d\n", a);
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 5)
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
